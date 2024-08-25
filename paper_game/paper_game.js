607bfb3b9d9a9c83b7a8b36ce82a6ca6ec1ca4f4
rbenim <bricharddarling@gmail.com>
2024-08-25 10:52:34 +0000

    js to determine winner of game

---
+++ paper_game/paper_game.js
@@ -0,0 +1,25 @@
+function getRandomComputerResult() {
+    const options = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+  }
+  
+  function hasPlayerWonTheRound(player, computer) {
+    if (player == "Rock" && computer == "Scissors"){
+      return true
+    }
+    else if (player == "Scissors" && computer == "Paper")
+    {
+      return true
+    }
+  
+  else if (player == "Paper" & computer == "Rock")
+    {
+      return true
+    }
+    else{return false}
+  
+  }
+  
+  console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
+  console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
