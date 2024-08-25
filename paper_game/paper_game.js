60ccb8e9d053eb839ee9fc86e843b8e7b28a691e
rbenim <bricharddarling@gmail.com>
2024-08-25 13:46:05 +0000

    reset scores

--- paper_game/paper_game.js
+++ paper_game/paper_game.js
@@ -32,14 +32,62 @@ function getRandomComputerResult() {
   const playerScoreSpanElement = document.getElementById("player-score");
   const computerScoreSpanElement = document.getElementById("computer-score");
   const roundResultsMsg = document.getElementById("results-msg");
+  const winnerMsgElement = document.getElementById("winner-msg");
+  const optionsContainer = document.querySelector(".options-container");
+  const resetGameBtn = document.getElementById("reset-game-btn");
   
   function showResults(userOption) {
     roundResultsMsg.innerText = getRoundResults(userOption);
+    computerScoreSpanElement.innerText = computerScore;
+    playerScoreSpanElement.innerText = playerScore;
   
-    // Update the score displays
+    // Check if there's a winner
+    if (playerScore === 3) {
+      winnerMsgElement.innerText = "Player has won the game!";
+      endGame();
+    } else if (computerScore === 3) {
+      winnerMsgElement.innerText = "Computer has won the game!";
+      endGame();
+    }
+  }
+  
+  function endGame() {
+    // Hide options and show reset button
+    optionsContainer.style.display = "none";
+    resetGameBtn.style.display = "block";
+  }
+  
+  function resetGame() {
+    // Reset scores and UI
+    playerScore = 0;
+    computerScore = 0;
     playerScoreSpanElement.innerText = playerScore;
     computerScoreSpanElement.innerText = computerScore;
+    roundResultsMsg.innerText = "";
+    winnerMsgElement.innerText = "";
   
-  };
+    // Show options and hide reset button
+    optionsContainer.style.display = "block";
+    resetGameBtn.style.display = "none";
+  }
   
-  showResults("Rock");
+  const rockBtn = document.getElementById("rock-btn");
+  const paperBtn = document.getElementById("paper-btn");
+  const scissorsBtn = document.getElementById("scissors-btn");
+  resetGameBtn.addEventListener("click", resetGame);
+  
+  rockBtn.addEventListener("click", function () {
+    showResults("Rock");
+  });
+  
+  paperBtn.addEventListener("click", function () {
+    showResults("Paper");
+  });
+  
+  scissorsBtn.addEventListener("click", function () {
+    showResults("Scissors");
+  });
+  
+  // Hide the reset button initially
+  resetGameBtn.style.display = "none";
+  
